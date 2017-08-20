b9ce060055fb84d38a1011f3ec0e9fcb82c774fd
samoen <samoen@gmail.com>
2017-08-20 16:56:18 +0800

    App name, question order

--- app/src/main/java/sam/quizzical/GlobalObject.kt
+++ app/src/main/java/sam/quizzical/GlobalObject.kt
@@ -10,15 +10,15 @@ object GlobalObject {
 
     fun SetDefaultQuestions(){
         questions = mutableListOf<Question>(
-                Question("Which one of these languages is NOT an official language of Switzerland?", "German", "Romanshe", "Italian", "Spanish", "French","", 4),
-                Question("Which of the following countries features a firearm on their national flag?", "Venezuela", "North Korea", "Vietnam", "Lichtenstein", "Mozambique","Guam",5),
                 Question("Which is the island famous for it's 12 metre tall carved stone heads?", "Christmas Island", "Tuvalu", "Easter Island","The Solomon Islands","Penguin Island","Luzon",3),
                 Question("Adjusted for inflation, which is the highest grossing movie of all time?","Gone with the Wind","Star Wars","The Exorcist","Titanic","Jaws","Snow White and the Seven Dwarfs",1),
                 Question("In which country/region did writing first emerge?", "China", "Mesopotamia", "Egypt", "Pakistan", "Babylonia","Central Africa",5),
                 Question("What is the smallest particle known to man?", "Proton", "Quark", "Neutrino", "Photon", "Atom","Molecule",3),
                 Question("Who is the worlds richest person?", "Warren Buffet", "Jeff Bezos", "Bill Gates", "Mark Zuckerberg", "Michael Bloomberg","Me",3),
+                Question("Which is the official language of Switzerland?", "German", "Romansh", "Italian", "French", "All of these","None of these", 5),
+                Question("Which of the following countries features a firearm on their national flag?", "Venezuela", "North Korea", "Vietnam", "Lichtenstein", "Mozambique","Guam",5),
                 Question("What is a String in programming?", "A sequence of numbers", "A sequence of characters", "A list of functions", "A list of variables", "A blob","A cloud",2),
-                Question("Who made this app?", "Sam Oen", "A Wildling", "Four moose", "Romanshe", "Jeff Bezos","Huh?",1)
+                Question("Who made this app?", "Sam Oen", "A Wildling", "Four moose", "Romansh", "Jeff Bezos","Huh?",1)
         )
     }
 
